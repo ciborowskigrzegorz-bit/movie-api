504a0ceca4c878ce56052b8cc0668790c5bbcf44
Mac <mac@MacBook-Air-Mac.local>
2025-08-03 13:24:52 +0200

    update secretkey

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY=supersecretkey
+SECRET_KEY = "supersecretkey"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
