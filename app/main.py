b4ce610b5a66924bf7de2cce2a0d0663c1a0fd68
Mac <mac@MacBook-Air-Mac.local>
2025-08-03 13:09:22 +0200

    update dotenv

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from app import models, database, routers
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # ładuje zmienne z pliku .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY=supersecretkey
 
 models.Base.metadata.create_all(bind=database.engine)
 
